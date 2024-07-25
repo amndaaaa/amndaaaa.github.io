a8639fb1f5cfb47c65e19159862fa76a24404841
Amanda Marques <amandinhammp004@gmail.com>
2024-07-25 17:32:41 -0300

    Atualizar o portfólio com link para HitMagazine

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Código JavaScript para interatividade, se necessário
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navLinks.forEach(link => link.classList.remove('active'));
+            link.classList.add('active');
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hitMagazineTitle = document.getElementById('hitMagazineTitle');
+    const hitMagazineContent = document.getElementById('hitMagazineContent');
+
+    hitMagazineTitle.addEventListener('click', () => {
+        hitMagazineContent.classList.toggle('show');
+    });
+});
+    
